b1e9cccfc5d1c3faf73da6bb5a362af6c18a81a4
Linming-XHL <LinmingXHL@qq.com>
2026-01-31 10:25:27 +0800

    new sponsor*2 （准备cos白子ing...）

--- src/config/sponsors.ts
+++ src/config/sponsors.ts
@@ -7,17 +7,14 @@ export interface Sponsor {
 export const sponsors: Sponsor[] = [
 	{
 		name: "风",
-		amount: 12,
-		date: "2025-10-01",
+		amount: 17,
 	},
 	{
 		name: "SkonYIJHD",
 		amount: 6,
-		date: "2025-10-15",
 	},
 	{
 		name: "启梦云",
 		amount: 3,
-		date: "2026-1-31",
 	},
 ];
