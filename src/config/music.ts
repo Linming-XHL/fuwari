32484f0b0003e01688fa3a6e0adf6e4c6fdc782e
Linming-XHL <LinmingXHL@qq.com>
2026-05-10 11:06:39 +0800

    feat(music): 新增多首Little End的歌曲到播放列表

--- src/config/music.ts
+++ src/config/music.ts
@@ -1,120 +1,270 @@
 export interface Song {
-  // 歌曲封面
-  cover: string;
-  // 歌名
-  title: string;
-  // 作者
-  artist: string;
-  // 音乐 mp3 地址
-  url: string;
+	// 歌曲封面
+	cover: string;
+	// 歌名
+	title: string;
+	// 作者
+	artist: string;
+	// 音乐 mp3 地址
+	url: string;
 }
 
 export const musicPlayerConfig = {
-  // 是否启用音乐播放器
-  enabled: true,
-  // 歌曲列表
-  songs: [
-    {
-      cover: "https://p1.music.126.net/z_rf3dqC5xHq5cqUchD8sw==/109951164937209894.jpg",
-      title: "Inspiration",
-      artist: "WavebeatsMusic",
-      url: "https://music.163.com/song/media/outer/url?id=1443334642.mp3",
-    },
-    {
-      cover: "https://p2.music.126.net/htlkqWdjglVH3rcNz-zwHg==/109951163017362740.jpg",
-      title: "葬花",
-      artist: "THT",
-      url: "https://music.163.com/song/media/outer/url?id=503426999.mp3",
-    },
-    {
-      cover: "https://p1.music.126.net/JyPsd_g00M-4mqXLLtHncw==/5984641790343690.jpg",
-      title: "The Way I Still Love You",
-      artist: "Reynard Silva",
-      url: "https://music.163.com/song/media/outer/url?id=2053342443.mp3",
-    },
-    {
-      cover: "https://p1.music.126.net/u8sBsK1P8RwXLe9zNhDUIw==/109951165643598348.jpg",
-      title: "Past Lives",
-      artist: "Martin Arteta,creamy,11:11 Music Group",
-      url: "https://music.163.com/song/media/outer/url?id=1813454565.mp3",
-    },
-    {
-      cover: "https://p1.music.126.net/vI69Lo8XO58-RwFk_NfwhQ==/109951163320249765.jpg",
-      title: "Snowdream",
-      artist: "VibeNova",
-      url: "https://music.163.com/song/media/outer/url?id=568709232.mp3",
-    },
-    {
-      cover: "https://p2.music.126.net/VxDvhpjxxHaUVx3RJeadOw==/109951169535119880.jpg",
-      title: "Collapsing World",
-      artist: "Lightscape",
-      url: "https://music.163.com/song/media/outer/url?id=2150413480.mp3",
-    },
-    {
-      cover: "https://p2.music.126.net/QFwzGhhmZDINqQ6iTlgaWg==/109951173127839710.jpg",
-      title: "室内系的TrackMaker",
-      artist: "Reze",
-      url: "https://music.163.com/song/media/outer/url?id=3374977079.mp3",
-    },
-    {
-      cover: "https://p1.music.126.net/vfArwmf4yUKmZhi-ZCwOXA==/109951166569406479.jpg",
-      title: "it's 6pm but I miss u already.",
-      artist: "BlueLee,Furyl,Siren",
-      url: "https://music.163.com/song/media/outer/url?id=1890756154.mp3",
-    },
-    {
-      cover: "https://p3.music.126.net/Y62X27B0BSy4iksxrGsZcg==/109951169746596184.jpg",
-      title: "Pure Ocean",
-      artist: "Jamvana",
-      url: "https://cdn.240900.xyz/music/pureocean.mp3",
-    },
-    {
-      cover: "https://p3.music.126.net/6mmoxID_rOAuGal0OoQi0g==/109951172949161096.jpg",
-      title: "Jackpot",
-      artist: "TheFatRat",
-      url: "https://music.163.com/song/media/outer/url?id=441612737.mp3",
-    },
-    {
-      cover: "https://p3.music.126.net/4hbR27M-uaKZVAOUojERTA==/109951168003842438.jpg",
-      title: "Alone",
-      artist: "Rentz",
-      url: "https://cdn.240900.xyz/music/alone-rentz.mp3",
-    },
-    {
-      cover: "https://p3.music.126.net/YvnCFnzoQLEZZ3BPlL_wvg==/109951169611905073.jpg",
-      title: "Hero",
-      artist: "Charlie Puth",
-      url: "https://cdn.240900.xyz/music/hero-c-p.mp3",
-    },
-    {
-      cover: "https://p3.music.126.net/LSNp2g4GIUaQBT1yvOq6XQ==/109951165983599325.jpg",
-      title: "All Falls Down",
-      artist: "Alan Walker/Noah Cyrus/Digital Farm Animals/Juliander",
-      url: "https://cdn.240900.xyz/music/allfallsdown.mp3",
-    },
-    {
-      cover: "https://p4.music.126.net/0JqA5WWFblL9dDvW6Ugutg==/109951168907663478.jpg",
-      title: "cyanic",
-      artist: "LhoU",
-      url: "https://music.163.com/song/media/outer/url?id=5319447.mp3",
-    },
-    {
-      cover: "https://p3.music.126.net/3RjnHJDUPNRa8bCAIz1h0w==/18155135998649267.jpg",
-      title: "Lake Arrowhead (Radio Mix)",
-      artist: "Nora En Pure",
-      url: "https://music.163.com/song/media/outer/url?id=477742336.mp3",
-    },
-    {
-      cover: "https://p3.music.126.net/9KbYbCdn2MmDJ96MKzL2Yw==/109951163457139434.jpg",
-      title: "Adventures",
-      artist: "Alex Skrindo",
-      url: "https://music.163.com/song/media/outer/url?id=33922813.mp3",
-    },
-    {
-      cover: "https://p4.music.126.net/fd1yRVqZ5R-jnkHf-I3pKg==/109951172949202233.jpg",
-      title: "Oblivion",
-      artist: "TheFatRat/Lola Blanc",
-      url: "https://music.163.com/song/media/outer/url?id=515540639.mp3",
-    },
-  ],
+	// 是否启用音乐播放器
+	enabled: true,
+	// 歌曲列表
+	songs: [
+		{
+			cover:
+				"https://p1.music.126.net/z_rf3dqC5xHq5cqUchD8sw==/109951164937209894.jpg",
+			title: "Inspiration",
+			artist: "WavebeatsMusic",
+			url: "https://music.163.com/song/media/outer/url?id=1443334642.mp3",
+		},
+		{
+			cover:
+				"https://p2.music.126.net/htlkqWdjglVH3rcNz-zwHg==/109951163017362740.jpg",
+			title: "葬花",
+			artist: "THT",
+			url: "https://music.163.com/song/media/outer/url?id=503426999.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/JyPsd_g00M-4mqXLLtHncw==/5984641790343690.jpg",
+			title: "The Way I Still Love You",
+			artist: "Reynard Silva",
+			url: "https://music.163.com/song/media/outer/url?id=2053342443.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/u8sBsK1P8RwXLe9zNhDUIw==/109951165643598348.jpg",
+			title: "Past Lives",
+			artist: "Martin Arteta,creamy,11:11 Music Group",
+			url: "https://music.163.com/song/media/outer/url?id=1813454565.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/vI69Lo8XO58-RwFk_NfwhQ==/109951163320249765.jpg",
+			title: "Snowdream",
+			artist: "VibeNova",
+			url: "https://music.163.com/song/media/outer/url?id=568709232.mp3",
+		},
+		{
+			cover:
+				"https://p2.music.126.net/VxDvhpjxxHaUVx3RJeadOw==/109951169535119880.jpg",
+			title: "Collapsing World",
+			artist: "Lightscape",
+			url: "https://music.163.com/song/media/outer/url?id=2150413480.mp3",
+		},
+		{
+			cover:
+				"https://p2.music.126.net/QFwzGhhmZDINqQ6iTlgaWg==/109951173127839710.jpg",
+			title: "室内系的TrackMaker",
+			artist: "Reze",
+			url: "https://music.163.com/song/media/outer/url?id=3374977079.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/vfArwmf4yUKmZhi-ZCwOXA==/109951166569406479.jpg",
+			title: "it's 6pm but I miss u already.",
+			artist: "BlueLee,Furyl,Siren",
+			url: "https://music.163.com/song/media/outer/url?id=1890756154.mp3",
+		},
+		{
+			cover:
+				"https://p3.music.126.net/Y62X27B0BSy4iksxrGsZcg==/109951169746596184.jpg",
+			title: "Pure Ocean",
+			artist: "Jamvana",
+			url: "https://cdn.240900.xyz/music/pureocean.mp3",
+		},
+		{
+			cover:
+				"https://p3.music.126.net/6mmoxID_rOAuGal0OoQi0g==/109951172949161096.jpg",
+			title: "Jackpot",
+			artist: "TheFatRat",
+			url: "https://music.163.com/song/media/outer/url?id=441612737.mp3",
+		},
+		{
+			cover:
+				"https://p3.music.126.net/4hbR27M-uaKZVAOUojERTA==/109951168003842438.jpg",
+			title: "Alone",
+			artist: "Rentz",
+			url: "https://cdn.240900.xyz/music/alone-rentz.mp3",
+		},
+		{
+			cover:
+				"https://p3.music.126.net/YvnCFnzoQLEZZ3BPlL_wvg==/109951169611905073.jpg",
+			title: "Hero",
+			artist: "Charlie Puth",
+			url: "https://cdn.240900.xyz/music/hero-c-p.mp3",
+		},
+		{
+			cover:
+				"https://p3.music.126.net/LSNp2g4GIUaQBT1yvOq6XQ==/109951165983599325.jpg",
+			title: "All Falls Down",
+			artist: "Alan Walker/Noah Cyrus/Digital Farm Animals/Juliander",
+			url: "https://cdn.240900.xyz/music/allfallsdown.mp3",
+		},
+		{
+			cover:
+				"https://p4.music.126.net/0JqA5WWFblL9dDvW6Ugutg==/109951168907663478.jpg",
+			title: "cyanic",
+			artist: "LhoU",
+			url: "https://music.163.com/song/media/outer/url?id=5319447.mp3",
+		},
+		{
+			cover:
+				"https://p3.music.126.net/3RjnHJDUPNRa8bCAIz1h0w==/18155135998649267.jpg",
+			title: "Lake Arrowhead (Radio Mix)",
+			artist: "Nora En Pure",
+			url: "https://music.163.com/song/media/outer/url?id=477742336.mp3",
+		},
+		{
+			cover:
+				"https://p3.music.126.net/9KbYbCdn2MmDJ96MKzL2Yw==/109951163457139434.jpg",
+			title: "Adventures",
+			artist: "Alex Skrindo",
+			url: "https://music.163.com/song/media/outer/url?id=33922813.mp3",
+		},
+		{
+			cover:
+				"https://p4.music.126.net/fd1yRVqZ5R-jnkHf-I3pKg==/109951172949202233.jpg",
+			title: "Oblivion",
+			artist: "TheFatRat/Lola Blanc",
+			url: "https://music.163.com/song/media/outer/url?id=515540639.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/hKqBtL1apL1VjHZyjOuAEQ==/109951165123308459.jpg",
+			title: "Here",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=1461445180.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/MlnTFb8Lla168ckih8p1jw==/109951164067078793.jpg",
+			title: "Forever Away",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=1346903862.mp3",
+		},
+		{
+			cover:
+				"https://p2.music.126.net/QO6ZDvOl7mhgXouzVNmnTg==/109951167886212384.jpg",
+			title: "Shooting Star",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=1315960996.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/hKqBtL1apL1VjHZyjOuAEQ==/109951164420598496.jpg",
+			title: "End Of The Abyss",
+			artist: "Little End/Pam_dinosaur",
+			url: "https://music.163.com/song/media/outer/url?id=1396261700.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/109951168039311342.jpg",
+			title: "City",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=1296641291.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/109951167886118894.jpg",
+			title: "Sky",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=544556228.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/109951167886206890.jpg",
+			title: "Hometown",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=530425152.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/109951166153280123.jpg",
+			title: "Ocean",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=1818940986.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/109951167886195333.jpg",
+			title: "Everything",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=1323390484.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/109951163999149688.jpg",
+			title: "Alive",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=1358775119.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/109951164657965525.jpg",
+			title: "Wind With Clouds",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=1419278912.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/109951170914788907.jpg",
+			title: "Line",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=2696820965.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/BYo6cyFHm_10Q-g3H-hbhg==/109951167560740316.jpg",
+			title: "Wet Cat Steps",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=1956596535.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/ScnT_6mJJ7XCJxVdDiAYDg==/109951164191589758.jpg",
+			title: "Memory",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=553895481.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/zw56vulsDpIK4RhR5XA76g==/109951167886139428.jpg",
+			title: "Summer",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=548888492.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/Ef64ztEAqaaJWOlFTMSKxw==/109951167886199422.jpg",
+			title: "Nowhere",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=557620630.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/ymlvtVBOUrREeXknKNtJKg==/109951167886152972.jpg",
+			title: "無錫",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=520180940.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/2IPZjpuAYurdHZwv0l8MLg==/109951167886141875.jpg",
+			title: "Journey",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=506657703.mp3",
+		},
+		{
+			cover:
+				"https://p1.music.126.net/cgwMrlccHeNaONJ7Hchq8Q==/109951163405198570.jpg",
+			title: "MINT SODA",
+			artist: "Little End",
+			url: "https://music.163.com/song/media/outer/url?id=541075125.mp3",
+		},
+	],
 };
