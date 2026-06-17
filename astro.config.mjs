9f5eef152d0797d64d4b12c462080d7abeec0a19
Linming-XHL <LinmingXHL@qq.com>
2026-06-17 17:45:39 +0800

    feat: Maple Mono cv01 禁用，代码框仅使用 Maple Mono

--- astro.config.mjs
+++ astro.config.mjs
@@ -84,7 +84,7 @@ export default defineConfig({
 				borderColor: "none",
 				codeFontSize: "0.875rem",
 				codeFontFamily:
-					"'Maple Mono', 'JetBrains Mono Variable', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+					"'Maple Mono'",
 				codeLineHeight: "1.5rem",
 				frames: {
 					editorBackground: "var(--codeblock-bg)",
